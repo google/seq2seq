8ca6e63a7b5b9aeedd4d4b05e8d4ac2fb7e613f7
Denny Britz <dennybritz@gmail.com>
2016-12-06 15:21:17 -0800

    Added test for HParamsParser

---
+++ seq2seq/test/hparams_parser_test.py
@@ -0,0 +1,24 @@
+"""
+Unit tests for HParamsParser.
+"""
+
+import unittest
+from seq2seq.training import HParamsParser
+
+class HParamsParserTest(unittest.TestCase):
+  """Test for HParamsParser class.
+  """
+
+  def test_parse(self):
+    default_params = {
+      "rnn_dim": 128,
+      "num_layers": 2,
+      "rnn_cell_type": "LSTM",
+      "dropout": 0.8
+    }
+    parser = HParamsParser(default_params)
+    final_params = parser.parse("rnn_dim=256,rnn_cell_type=GRU,dropout=0.77")
+    self.assertEqual(final_params["rnn_dim"], 256)
+    self.assertEqual(final_params["rnn_cell_type"], "GRU")
+    self.assertEqual(final_params["dropout"], 0.77)
+    self.assertEqual(final_params["num_layers"], 2)
