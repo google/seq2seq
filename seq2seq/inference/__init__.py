4748451cb5ae94dd3f4124483a32ab071ee333d0
Denny Britz <dennybritz@gmail.com>
2016-12-24 07:05:59 -0800

    Move files

---
+++ seq2seq/inference/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from seq2seq.inference.inference import *
+import seq2seq.inference.beam_search
