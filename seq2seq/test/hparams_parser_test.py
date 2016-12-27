cf5f714dcae032fc220f41c8dd1afba9e7da7b8e
Denny Britz <dennybritz@gmail.com>
2016-12-27 10:48:27 -0800

    Refactor hparams parser to typecast

--- seq2seq/test/hparams_parser_test.py
+++ seq2seq/test/hparams_parser_test.py
@@ -39,6 +39,37 @@ class HParamsParserTest(unittest.TestCase):
     self.assertEqual(final_params["dropout"], 0.77)
     self.assertEqual(final_params["num_layers"], 2)
 
+  def test_parse_without_defaults(self):
+    default_params = {
+        "rnn_dim": 128,
+        "num_layers": 2,
+        "rnn_cell_type": "LSTM",
+        "dropout": 0.8
+    }
+    parser = HParamsParser(default_params)
+    final_params = parser.parse(
+        "\n".join(["rnn_dim=256,", "rnn_cell_type=GRU,", "dropout=0.77"]),
+        with_defaults=False)
+    self.assertEqual(final_params["rnn_dim"], 256)
+    self.assertEqual(final_params["rnn_cell_type"], "GRU")
+    self.assertEqual(final_params["dropout"], 0.77)
+    self.assertNotIn("num_layers", final_params)
+
+  def test_parse_with_float(self):
+    default_params = {
+        "rnn_dim": 128,
+        "num_layers": 2,
+        "rnn_cell_type": "LSTM",
+        "dropout": 0.8
+    }
+    parser = HParamsParser(default_params)
+    final_params = parser.parse(
+        "\n".join(["rnn_dim=256.00,", "rnn_cell_type=GRU,", "dropout=0.77"]))
+    self.assertEqual(final_params["rnn_dim"], 256)
+    self.assertEqual(final_params["rnn_cell_type"], "GRU")
+    self.assertEqual(final_params["dropout"], 0.77)
+    self.assertEqual(final_params["num_layers"], 2)
+
 
 if __name__ == '__main__':
   unittest.main()
