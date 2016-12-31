8da3b2d7da22099c43bab203b5eca3a7c243e87d
sebastianruder <ruder.sebastian@gmail.com>
2016-12-31 12:29:53 +0000

    Fixed input to NLTK bleu_score, added update_op via streaming_mean

--- seq2seq/training/metrics.py
+++ seq2seq/training/metrics.py
@@ -11,7 +11,8 @@ from tensorflow.contrib import metrics
 from tensorflow.contrib.learn.python.learn import metric_spec
 
 import numpy as np
-from nltk.translate import bleu
+from nltk.translate import bleu_score
+
 
 def streaming_log_perplexity():
   """Creates a MetricSpec that calculates the log perplexity.
@@ -34,17 +35,27 @@ def streaming_bleu():
   """Creates a MetricSpec that calculates the BLEU score.
   """
 
-  def calculate_bleu(labels, predictions):
+  def calculate_bleu(target_ids, predictions):
     """Calculates the BLEU score using NLTK."""
-    score = bleu(labels.astype(str), predictions.astype(str))
+    # NLTK expects the inputs as strings; we also use corpus_bleu
+    # to calculate the BLEU score for all sentences in a batch
+    # and wrap the target_ids in a list, as NLTK expects potentially
+    # multiple reference translations per example
+    str_target_ids = [[list(sentence_labels.astype(str))]
+                      for sentence_labels in list(target_ids)]
+    str_predictions = [list(sentence_predictions.astype(str))
+                       for sentence_predictions in list(predictions)]
+    score = bleu_score.corpus_bleu(str_target_ids, str_predictions)
     return np.array(score, dtype=np.float32)
 
-  def bleu_metric(labels, predictions):
+  def bleu_metric(target_ids, predictions):
     """Calculates the BLEU score based on labels and predictions"""
-    return tf.py_func(calculate_bleu, [labels, predictions],
-                      [tf.float32])
+    print('Calculating BLEU metric...')
+    bleu_scores = tf.py_func(calculate_bleu, [target_ids, predictions],
+                            [tf.float32])
+    return metrics.streaming_mean(bleu_scores)
 
   return metric_spec.MetricSpec(
       metric_fn=bleu_metric,
-      label_key="labels",
+      label_key="target_ids",
       prediction_key="predictions")
