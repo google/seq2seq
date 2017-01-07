6f8f73034ae86e1ac1e5f56511fb389b6bfdd53d
Denny Britz <dennybritz@gmail.com>
2017-01-07 07:57:13 -0800

    Encoding to utils file

--- seq2seq/training/utils.py
+++ seq2seq/training/utils.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
+
 """Miscellaneous training utility functions.
 """
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from __future__ import unicode_literals
 
 import os
 import re
