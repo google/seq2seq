33e062df4d7af703879eacdd8f4c15d86d8af258
Denny Britz <dennybritz@gmail.com>
2017-01-11 13:05:14 -0800

    Remove old code

--- seq2seq/test/train_utils_test.py
+++ seq2seq/test/train_utils_test.py
@@ -70,36 +70,6 @@ class TestTrainOptions(tf.test.TestCase):
   def test_partial_read_write(self):
     pass
 
-# class TestHparamsReadWrite(tf.test.TestCase):
-#   """Tests reading and writing of HParam values
-#   """
-
-#   def setUp(self):
-#     super(TestHparamsReadWrite, self).setUp()
-#     self.hparams = {
-#         "rnn_dim": 128,
-#         "num_layers": 2,
-#         "rnn_cell_type": "LSTM",
-#         "dropout": 0.8
-#     }
-#     self.parser = HParamsParser(self.hparams)
-
-#   def test_write(self):
-#     file = tempfile.NamedTemporaryFile()
-#     training_utils.write_hparams(self.hparams, file.name)
-#     lines = file.read()
-#     self.assertEqual(
-#         lines.decode(),
-#         "dropout=0.8\nnum_layers=2\nrnn_cell_type=LSTM\nrnn_dim=128")
-#     file.close()
-
-#   def test_write_read(self):
-#     file = tempfile.NamedTemporaryFile()
-#     training_utils.write_hparams(self.hparams, file.name)
-#     params_str = training_utils.read_hparams(file.name)
-#     final_params = self.parser.parse(params_str)
-#     self.assertEqual(final_params, self.hparams)
-
 
 class TestInputFn(tf.test.TestCase):
   """Tests create_input_fn"""
