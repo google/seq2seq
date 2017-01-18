3c3431342db4c4824e127b76c8a582ad8e52740c
Denny Britz <dennybritz@gmail.com>
2017-01-17 19:54:50 -0800

    Fix typo

--- seq2seq/training/hparams_parser.py
+++ seq2seq/training/hparams_parser.py
@@ -28,7 +28,7 @@ class HParamsParser(object):
     if with_defaults:
       final_params.update(self.default_params.copy())
 
-    # Split paraeters
+    # Split parameters
     params = re.split(r",(?!\s\")", params_str)
     params = [param.split("=") for param in params]
     params = dict([(k.strip(), v.strip()) for k, v in params])
