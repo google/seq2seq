cb76b94d48d521fae5ea41f39cfa44c7dcbf3ff1
Denny Britz <dennybritz@gmail.com>
2017-02-10 13:02:00 -0800

    Improve gigaword processing script

--- bin/data/gigaword_prepare.sh
+++ bin/data/gigaword_prepare.sh
@@ -7,9 +7,11 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 DATA_DIR=${DATA_DIR:-$HOME/nmt_data}
 echo "Writing to ${DATA_DIR}. To change this, set the DATA_DIR environment variable."
 
-OUTPUT_DIR=${DATA_DIR}/gigaword
-mkdir -p $OUTPUT_DIR
+NUM_ARTICLE_SENTENCES=${NUM_ARTICLE_SENTENCES:-2}
+echo "Using first $NUM_ARTICLE_SENTENCES from the article."
 
+OUTPUT_DIR=${DATA_DIR}/gigaword_${NUM_ARTICLE_SENTENCES}sent/
+mkdir -p $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR/extracted
 
 # Unzip all files
@@ -18,7 +20,7 @@ find  $GIGAWORD_DIR -name *.gz | xargs gunzip
 # Process all data files
 for f in $(find $GIGAWORD_DIR -type f | grep data/); do
   outfile="$OUTPUT_DIR/extracted/$(basename $f)"
-  $BASE_DIR/bin/data/gigaword_extract.py -f $f -o $outfile
+  $BASE_DIR/bin/data/gigaword_extract.py -n $NUM_ARTICLE_SENTENCES -f $f -o $outfile
   echo "$(wc -l ${outfile}/sources.txt)"
   echo "$(wc -l ${outfile}/targets.txt)"
 done
@@ -43,19 +45,19 @@ ${DATA_DIR}/mosesdecoder/scripts/training/clean-corpus-n.perl \
 
 # Lowercase data
 # A lot of the headlines in Gigaword are all caps
-tr '[:upper:]' '[:lower:]' < ${OUTPUT_DIR}/combined.clean.sources > ${OUTPUT_DIR}/combined.lc.sources
-tr '[:upper:]' '[:lower:]' < ${OUTPUT_DIR}/combined.clean.targets > ${OUTPUT_DIR}/combined.lc.targets
+tr '[:upper:]' '[:lower:]' < ${OUTPUT_DIR}/combined.clean.sources > ${OUTPUT_DIR}/combined.clean.lc.sources
+tr '[:upper:]' '[:lower:]' < ${OUTPUT_DIR}/combined.clean.targets > ${OUTPUT_DIR}/combined.clean.lc.targets
 
 # Tokenize data
 ${DATA_DIR}/mosesdecoder/scripts/tokenizer/tokenizer.perl \
   -q -l en -threads 8 \
-  < ${OUTPUT_DIR}/combined.lc.sources \
-  > ${OUTPUT_DIR}/combined.tok.sources
+  < ${OUTPUT_DIR}/combined.clean.lc.sources \
+  > ${OUTPUT_DIR}/combined.clean.lc.tok.sources
 
 ${DATA_DIR}/mosesdecoder/scripts/tokenizer/tokenizer.perl \
   -q -l en -threads 8 \
-  < ${OUTPUT_DIR}/combined.lc.targets \
-  > ${OUTPUT_DIR}/combined.tok.targets
+  < ${OUTPUT_DIR}/combined.clean.lc.targets \
+  > ${OUTPUT_DIR}/combined.clean.lc.tok.targets
 
 
 # Shuffle data in parallel
@@ -63,39 +65,39 @@ ${DATA_DIR}/mosesdecoder/scripts/tokenizer/tokenizer.perl \
 mkfifo  ${OUTPUT_DIR}/random1 ${OUTPUT_DIR}/random2
 tee ${OUTPUT_DIR}/random1 ${OUTPUT_DIR}/random2 < /dev/urandom > /dev/null &
 shuf --random-source=${OUTPUT_DIR}/random1 \
-  ${OUTPUT_DIR}/combined.tok.sources > ${OUTPUT_DIR}/combined.tok.shuf.sources &
+  ${OUTPUT_DIR}/combined.clean.lc.tok.sources > ${OUTPUT_DIR}/combined.clean.lc.tok.shuf.sources &
 shuf --random-source ${OUTPUT_DIR}/random2 \
-  ${OUTPUT_DIR}/combined.tok.targets > ${OUTPUT_DIR}/combined.tok.shuf.targets &
+  ${OUTPUT_DIR}/combined.clean.lc.tok.targets > ${OUTPUT_DIR}/combined.clean.lc.tok.shuf.targets &
 wait
 rm ${OUTPUT_DIR}/random1 ${OUTPUT_DIR}/random2
 
 
 # Split into train/dev/test
-tail -n +20000 ${OUTPUT_DIR}/combined.tok.shuf.sources > ${OUTPUT_DIR}/train.sources
-tail -n +20000 ${OUTPUT_DIR}/combined.tok.shuf.targets > ${OUTPUT_DIR}/train.targets
-head -n 10000 ${OUTPUT_DIR}/combined.tok.shuf.sources > ${OUTPUT_DIR}/dev.sources
-head -n 10000 ${OUTPUT_DIR}/combined.tok.shuf.targets > ${OUTPUT_DIR}/dev.targets
-head -n 20000 ${OUTPUT_DIR}/combined.tok.shuf.sources | tail -n 10000 > ${OUTPUT_DIR}/test.sources
-head -n 20000 ${OUTPUT_DIR}/combined.tok.shuf.targets | tail -n 10000 > ${OUTPUT_DIR}/test.targets
+tail -n +20000 ${OUTPUT_DIR}/combined.clean.lc.tok.shuf.sources > ${OUTPUT_DIR}/train.lc.tok.sources
+tail -n +20000 ${OUTPUT_DIR}/combined.clean.lc.tok.shuf.targets > ${OUTPUT_DIR}/train.lc.tok.targets
+head -n 10000 ${OUTPUT_DIR}/combined.clean.lc.tok.shuf.sources > ${OUTPUT_DIR}/dev.lc.tok.sources
+head -n 10000 ${OUTPUT_DIR}/combined.clean.lc.tok.shuf.targets > ${OUTPUT_DIR}/dev.lc.tok.targets
+head -n 20000 ${OUTPUT_DIR}/combined.clean.lc.tok.shuf.sources | tail -n 10000 > ${OUTPUT_DIR}/test.lc.tok.sources
+head -n 20000 ${OUTPUT_DIR}/combined.clean.lc.tok.shuf.targets | tail -n 10000 > ${OUTPUT_DIR}/test.lc.tok.targets
 
-# Learn BPE
+# Get BPE repo
 if [ ! -d "${DATA_DIR}/subword-nmt" ]; then
   git clone https://github.com/rsennrich/subword-nmt.git "${DATA_DIR}/subword-nmt"
 fi
 
 BPE_MERGE_OPS=32000
 echo "Learning BPE with merge_ops=${BPE_MERGE_OPS}. This may take a while..."
-cat "${OUTPUT_DIR}/train.sources" "${OUTPUT_DIR}/train.targets" | \
+cat "${OUTPUT_DIR}/train.lc.tok.sources" "${OUTPUT_DIR}/train.lc.tok.targets" | \
     ${DATA_DIR}/subword-nmt/learn_bpe.py -s $BPE_MERGE_OPS > "${OUTPUT_DIR}/bpe.${BPE_MERGE_OPS}"
 
 echo "Apply BPE with merge_ops=${BPE_MERGE_OPS}..."
 for f in train dev test; do
   ${DATA_DIR}/subword-nmt/apply_bpe.py -c "${OUTPUT_DIR}/bpe.${BPE_MERGE_OPS}" \
-    < ${OUTPUT_DIR}/${f}.sources > ${OUTPUT_DIR}/${f}.bpe.${BPE_MERGE_OPS}.sources
+    < ${OUTPUT_DIR}/${f}.lc.tok.sources > ${OUTPUT_DIR}/${f}.lc.tok.bpe.${BPE_MERGE_OPS}.sources
   ${DATA_DIR}/subword-nmt/apply_bpe.py -c "${OUTPUT_DIR}/bpe.${BPE_MERGE_OPS}" \
-    < ${OUTPUT_DIR}/${f}.targets > ${OUTPUT_DIR}/${f}.bpe.${BPE_MERGE_OPS}.targets
+    < ${OUTPUT_DIR}/${f}.lc.tok.targets > ${OUTPUT_DIR}/${f}.lc.tok.bpe.${BPE_MERGE_OPS}.targets
 done
 
 # Learn vocabulary
-cat "${OUTPUT_DIR}/train.bpe.${BPE_MERGE_OPS}.sources" "${OUTPUT_DIR}/train.bpe.${BPE_MERGE_OPS}.targets" \
+cat "${OUTPUT_DIR}/train.lc.tok.bpe.${BPE_MERGE_OPS}.sources" "${OUTPUT_DIR}/train.lc.tok.bpe.${BPE_MERGE_OPS}.targets" \
   | ${DATA_DIR}/subword-nmt/get_vocab.py | cut -f1 -d ' ' > "${OUTPUT_DIR}/vocab.bpe.${BPE_MERGE_OPS}"
