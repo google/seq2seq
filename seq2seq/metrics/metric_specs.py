a1a2d4b01bbcfc07db6139963a21f38fd8fc0c61
Denny Britz <dennybritz@gmail.com>
2017-02-10 10:42:13 -0800

    Use PyRouge for rouge evaluation

--- seq2seq/metrics/metric_specs.py
+++ seq2seq/metrics/metric_specs.py
@@ -143,33 +143,45 @@ class BleuMetricSpec(TextMetricSpec):
         lowercase=False)
 
 
-class RougeNMetricSpec(TextMetricSpec):
+class RougeMetricSpec(TextMetricSpec):
   """Calculates BLEU score using the Moses multi-bleu.perl script.
   """
-  def __init__(self, n, separator=" ", eos_token="SEQUENCE_END"):
-    super(RougeNMetricSpec, self).__init__(
-        "rouge_{}_metric".format(n), separator, eos_token)
-    self.n = n #pylint: disable=C0103
+  def __init__(self, metric_name, **kwargs):
+    super(RougeMetricSpec, self).__init__(metric_name, **kwargs)
+    self.metric_name = metric_name
 
   def metric_fn(self, hypotheses, references):
     if not hypotheses or not references:
       return np.float32(0.0)
-    return np.float32(rouge.rouge_n(hypotheses, references, n=self.n))
+    return np.float32(rouge.rouge(hypotheses, references)[self.metric_name])
 
 
-class RougeLMetricSpec(TextMetricSpec):
-  """Calculates BLEU score using the Moses multi-bleu.perl script.
-  """
-  def __init__(self, separator=" ", eos_token="SEQUENCE_END"):
-    super(RougeLMetricSpec, self).__init__(
-        "rouge_l_sent_metric", separator, eos_token)
+# class Rouge2MetricSpec(TextMetricSpec):
+#   """Calculates BLEU score using the Moses multi-bleu.perl script.
+#   """
+#   def __init__(self, KK separator=" ", eos_token="SEQUENCE_END"):
+#     super(Rouge1MetricSpec, self).__init__(
+#         "rouge_2_f_score".format(n), separator, eos_token)
 
-  def metric_fn(self, hypotheses, references):
-    if not hypotheses or not references:
-      return np.float32(0.0)
-    return np.float32(
-        rouge.rouge_l_sentence_level(
-            hypotheses, references))
+#   def metric_fn(self, hypotheses, references):
+#     if not hypotheses or not references:
+#       return np.float32(0.0)
+#     return np.float32(rouge.rouge(hypotheses, references)["rouge_2_f_score"])
+
+
+# class RougeLMetricSpec(TextMetricSpec):
+#   """Calculates BLEU score using the Moses multi-bleu.perl script.
+#   """
+#   def __init__(self, separator=" ", eos_token="SEQUENCE_END"):
+#     super(RougeLMetricSpec, self).__init__(
+#         "rouge_l_sent_metric", separator, eos_token)
+
+#   def metric_fn(self, hypotheses, references):
+#     if not hypotheses or not references:
+#       return np.float32(0.0)
+#     return np.float32(
+#         rouge.rouge_l_sentence_level(
+#            hypotheses, references))
 
 class LogPerplexityMetricSpec(metric_spec.MetricSpec):
   """A MetricSpec to calculate straming log perplexity"""
--- seq2seq/test/metrics_test.py
+++ seq2seq/test/metrics_test.py
@@ -14,8 +14,8 @@ import tensorflow as tf
 
 from seq2seq.metrics import bleu
 from seq2seq.metrics import rouge
-from seq2seq.metrics.metric_specs import BleuMetricSpec, RougeNMetricSpec
-from seq2seq.metrics.metric_specs import RougeLMetricSpec
+from seq2seq.metrics.metric_specs import BleuMetricSpec
+from seq2seq.metrics.metric_specs import RougeMetricSpec
 
 
 class TestMosesBleu(tf.test.TestCase):
@@ -99,123 +99,46 @@ class TestBleuMetricSpec(TestTextMetricSpec):
         expected_scores=[100.0, 69.19]
     )
 
-class TestRougeNMetricSpec(TestTextMetricSpec):
+class TestRougeMetricSpec(TestTextMetricSpec):
   """Tests the `PrintModelAnalysisHook` hook"""
 
-  def test_rouge_n(self):
-    metric_spec = RougeNMetricSpec(n=2)
+  def test_rouge_1_f_score(self):
+    metric_spec = RougeMetricSpec("rouge_1_f_score")
     return self._test_metric_spec(
         metric_spec=metric_spec,
         hyps=["A B C D E F", "A B C D E F"],
         refs=["A B C D E F", "A B A D E F"],
-        expected_scores=[1.0, 0.75]
+        expected_scores=[1.0, 0.916]
     )
 
-class TestRougeLMetricSpec(TestTextMetricSpec):
-  """Tests the `PrintModelAnalysisHook` hook"""
-
-  def test_rouge_l(self):
-    metric_spec = RougeLMetricSpec()
+  def test_rouge_2_f_score(self):
+    metric_spec = RougeMetricSpec("rouge_2_f_score")
     return self._test_metric_spec(
         metric_spec=metric_spec,
         hyps=["A B C D E F", "A B C D E F"],
         refs=["A B C D E F", "A B A D E F"],
-        expected_scores=[1.0, 0.916669]
+        expected_scores=[1.0, 0.8]
     )
 
+  def test_rouge_l_f_score(self):
+    metric_spec = RougeMetricSpec("rouge_l_f_score")
+    return self._test_metric_spec(
+        metric_spec=metric_spec,
+        hyps=["A B C D E F", "A B C D E F"],
+        refs=["A B C D E F", "A B A D E F"],
+        expected_scores=[1.0, 0.916]
+    )
 
 
 class TestRougeMetric(tf.test.TestCase):
-  def test_get_ngrams(self):
-    self.assertTrue(not rouge._get_ngrams(3, ""))
-    correct_ngrams = [("t", "e"), ("e", "s"), ("s", "t"),
-                      ("t", "i"), ("i", "n"), ("n", "g")]
-    found_ngrams = rouge._get_ngrams(2, "testing")
-    self.assertEqual(len(correct_ngrams), len(found_ngrams))
-    for ngram in correct_ngrams:
-      self.assertTrue(ngram in found_ngrams)
-
-  def test_split_into_words(self):
-    sentences1 = ["One two two Two Three"]
-    self.assertEqual(
-        ["One", "two", "two", "Two", "Three"],
-        rouge._split_into_words(sentences1))
-
-  def test_get_word_ngrams(self):
-    sentences = ["This is a test"]
-    correct_ngrams = [("This", "is"), ("is", "a"), ("a", "test")]
-    found_ngrams = rouge._get_word_ngrams(2, sentences)
-    for ngram in correct_ngrams:
-      self.assertTrue(ngram in found_ngrams)
-
-  def test_len_lcs(self):
-      self.assertEqual(rouge._len_lcs("1234", "1224533324"), 4)
-      self.assertEqual(rouge._len_lcs("thisisatest", "testing123testing"), 7)
-
-  def test_recon_lcs(self):
-    self.assertEqual(
-        rouge._recon_lcs("1234", "1224533324"),
-        ("1", "2", "3", "4"))
-    self.assertEqual(
-        rouge._recon_lcs("thisisatest", "testing123testing"),
-        ("t", "s", "i", "t", "e", "s", "t"))
-
-  def test_rouge_n(self):
-    candidate = ["pulses may ease schizophrenic voices"]
-
-    reference1 = ["magnetic pulse series sent through brain may " \
-      "ease schizophrenic voices"]
-
-    reference2 = ["yale finds magnetic stimulation some relief to " \
-      "schizophrenics imaginary voices"]
-
-    self.assertAlmostEqual(rouge.rouge_n(candidate, reference1, 1), 4/10)
-    self.assertAlmostEqual(rouge.rouge_n(candidate, reference2, 1), 1/10)
-
-    self.assertAlmostEqual(rouge.rouge_n(candidate, reference1, 2), 3/9)
-    self.assertAlmostEqual(rouge.rouge_n(candidate, reference2, 2), 0/9)
-
-    self.assertAlmostEqual(rouge.rouge_n(candidate, reference1, 3), 2/8)
-    self.assertAlmostEqual(rouge.rouge_n(candidate, reference2, 3), 0/8)
-
-    self.assertAlmostEqual(rouge.rouge_n(candidate, reference1, 4), 1/7)
-    self.assertAlmostEqual(rouge.rouge_n(candidate, reference2, 4), 0/7)
-
-    # These tests will apply when multiple reference summaries can be input
-    # self.assertAlmostEqual(
-    #     rouge.rouge_n(candidate, [reference1, reference2], 1), 5/20)
-    # self.assertAlmostEqual(
-    #     rouge.rouge_n(candidate, [reference1, reference2], 2), 3/18)
-    # self.assertAlmostEqual(
-    #     rouge.rouge_n(candidate, [reference1, reference2], 3), 2/16)
-    # self.assertAlmostEqual(
-    #     rouge.rouge_n(candidate, [reference1, reference2], 4), 1/14)
-
-
-  def test_rouge_l_sentence_level(self):
-    reference = ["police killed the gunman"]
-    candidate1 = ["police kill the gunman"]
-    candidate2 = ["the gunman kill police"]
-    candidate3 = ["the gunman police killed"]
-
-    self.assertAlmostEqual(
-        rouge.rouge_l_sentence_level(candidate1, reference), 3/4)
-    self.assertAlmostEqual(
-        rouge.rouge_l_sentence_level(candidate2, reference), 2/4)
-    self.assertAlmostEqual(
-        rouge.rouge_l_sentence_level(candidate3, reference), 2/4)
-
-  def test_union_lcs(self):
-    reference = ["one two three four five"]
-    candidates = ["one two six seven eight", "one three eight nine five"]
-    self.assertAlmostEqual(rouge._union_lcs(candidates, reference[0]), 4/5)
-
-  def test_rouge_l_summary_level(self):
-    reference = ["one two three four five", "one two three four five"]
-    candidates = ["one two six seven eight", "one three eight nine five"]
-    self.assertAlmostEqual(
-        rouge.rouge_l_summary_level(candidates, reference), 0.16)
-
-
-if __name__ == "__main__":
-  tf.test.main()
+  """Tests the RougeMetric"""
+  def test_rouge(self):
+    hypotheses = np.array([
+        "The brown fox jumps over the dog 笑",
+        "The brown fox jumps over the dog 2 笑"])
+    references = np.array([
+        "The quick brown fox jumps over the lazy dog 笑",
+        "The quick brown fox jumps over the lazy dog 笑"])
+    output = rouge.rouge(hypotheses, references)
+    np.testing.assert_almost_equal(output["rouge_1_f_score"], 0.84926)
+    np.testing.assert_almost_equal(output["rouge_2_f_score"], 0.55238)
