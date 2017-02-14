9d215a04d4c2f580df98cf481fb63ca79362317d
Denny Britz <dennybritz@gmail.com>
2017-02-13 18:00:46 -0800

    Added TFRecordInputPipeline class

--- seq2seq/data/input_pipeline.py
+++ seq2seq/data/input_pipeline.py
@@ -15,6 +15,7 @@ import six
 import tensorflow as tf
 
 from seq2seq.data import split_tokens_decoder, parallel_data_provider
+from tensorflow.contrib.slim.python.slim.data import tfexample_decoder
 
 @six.add_metaclass(abc.ABCMeta)
 class InputPipeline(object):
@@ -38,6 +39,80 @@ class InputPipeline(object):
     item_values = data_provider.get(list(data_provider.list_items()))
     items_dict = dict(zip(data_provider.list_items(), item_values))
     return items_dict
+
+
+class TFRecordInputPipeline(InputPipeline):
+  def __init__(self, files,
+               source_field="source", target_field="target",
+               source_delimiter=" ", target_delimiter=" ",
+               shuffle=False, num_epochs=None):
+    super(TFRecordInputPipeline, self).__init__(shuffle, num_epochs)
+    self._files = files
+    self._source_field = source_field
+    self._target_field = target_field
+    self._source_delimiter = source_delimiter
+    self._target_delimiter = target_delimiter
+
+  def make_data_provider(self, **kwargs):
+
+    splitter_source = split_tokens_decoder.SplitTokensDecoder(
+        tokens_feature_name="source_tokens",
+        length_feature_name="source_len",
+        append_token="SEQUENCE_END",
+        delimiter=self._source_delimiter)
+
+    splitter_target = split_tokens_decoder.SplitTokensDecoder(
+        tokens_feature_name="target_tokens",
+        length_feature_name="target_len",
+        prepend_token="SEQUENCE_START",
+        append_token="SEQUENCE_END",
+        delimiter=self._target_delimiter)
+
+    keys_to_features = {
+        self._source_field: tf.FixedLenFeature((), tf.string),
+        self._target_field: tf.FixedLenFeature((), tf.string, default_value="")
+    }
+
+    items_to_handlers = {}
+    items_to_handlers["source_tokens"] = tfexample_decoder.ItemHandlerCallback(
+        keys=[self._source_field],
+        func=lambda dict: splitter_source.decode(
+            dict[self._source_field], ["source_tokens"])[0])
+    items_to_handlers["source_len"] = tfexample_decoder.ItemHandlerCallback(
+        keys=[self._source_field],
+        func=lambda dict: splitter_source.decode(
+            dict[self._source_field], ["source_len"])[0])
+    items_to_handlers["target_tokens"] = tfexample_decoder.ItemHandlerCallback(
+        keys=[self._target_field],
+        func=lambda dict: splitter_target.decode(
+            dict[self._target_field], ["target_tokens"])[0])
+    items_to_handlers["target_len"] = tfexample_decoder.ItemHandlerCallback(
+        keys=[self._target_field],
+        func=lambda dict: splitter_target.decode(
+            dict[self._target_field], ["target_len"])[0])
+
+    decoder = tfexample_decoder.TFExampleDecoder(
+        keys_to_features, items_to_handlers)
+
+    dataset = tf.contrib.slim.dataset.Dataset(
+        data_sources=self._files,
+        reader=tf.TFRecordReader,
+        decoder=decoder,
+        num_samples=None,
+        items_to_descriptions={})
+
+    return tf.contrib.slim.dataset_data_provider.DatasetDataProvider(
+        dataset=dataset, shuffle=self._shuffle, num_epochs=self._num_epochs,
+        **kwargs)
+
+  @property
+  def feature_keys(self):
+    return set(["source_tokens", "source_len"])
+
+  @property
+  def label_keys(self):
+    return set(["target_tokens", "target_len"])
+
 
 
 class ParallelTextInputPipeline(InputPipeline):
--- seq2seq/test/input_pipeline_test.py
+++ seq2seq/test/input_pipeline_test.py
@@ -16,6 +16,47 @@ import numpy as np
 from seq2seq.data import input_pipeline
 from seq2seq.test import utils as test_utils
 
+
+class TFRecordsInputPipelineTest(tf.test.TestCase):
+  """
+  Tests Data Provider operations.
+  """
+
+  def setUp(self):
+    super(TFRecordsInputPipelineTest, self).setUp()
+    tf.logging.set_verbosity(tf.logging.INFO)
+
+  def test_pipeline(self):
+    tfrecords_file = test_utils.create_temp_tfrecords(
+        sources=["Hello World . 笑"], targets=["Bye 泣"])
+
+    pipeline = input_pipeline.TFRecordInputPipeline(
+        files=[tfrecords_file.name],
+        source_field="source",
+        target_field="target",
+        num_epochs=5,
+        shuffle=False)
+
+    data_provider = pipeline.make_data_provider()
+
+    features = pipeline.read_from_data_provider(data_provider)
+
+    with self.test_session() as sess:
+      sess.run(tf.global_variables_initializer())
+      sess.run(tf.local_variables_initializer())
+      with tf.contrib.slim.queues.QueueRunners(sess):
+        res = sess.run(features)
+
+    self.assertEqual(res["source_len"], 5)
+    self.assertEqual(res["target_len"], 4)
+    np.testing.assert_array_equal(
+        np.char.decode(res["source_tokens"].astype("S"), "utf-8"),
+        ["Hello", "World", ".", "笑", "SEQUENCE_END"])
+    np.testing.assert_array_equal(
+        np.char.decode(res["target_tokens"].astype("S"), "utf-8"),
+        ["SEQUENCE_START", "Bye", "泣", "SEQUENCE_END"])
+
+
 class ParallelTextInputPipelineTest(tf.test.TestCase):
   """
   Tests Data Provider operations.
