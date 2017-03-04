0ef5c0cf5753c76f92c08b5b3d2219fa893f2729
Denny Britz <dennybritz@gmail.com>
2017-03-04 11:52:32 -0800

    Add pooling encoder

---
+++ seq2seq/encoders/pooling_encoder.py
@@ -0,0 +1,117 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+An encoder that pools over embeddings, as described in
+https://arxiv.org/abs/1611.02344.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from pydoc import locate
+
+import tensorflow as tf
+
+from seq2seq.encoders.encoder import Encoder, EncoderOutput
+
+
+def _create_position_embedding(embedding_dim, num_positions, lengths, maxlen):
+  """Creates posiiton embeddings.
+
+  Args:
+    embedding_dim: Dimensionality of the embeddings. An integer.
+    num_positions: The number of positions to be embedded. For example,
+      if you have inputs of length up to 100, this should be 100. An integer.
+    lengths: The lengths of the inputs to create position embeddings for.
+      An int32 tensor of shape `[batch_size]`.
+    maxlen: The maximum length of the input sequence to create positon
+      embeddings for. An int32 tensor.
+
+  Returns:
+    A tensor of shape `[batch_size, maxlen, embedding_dim]` that contains
+    embeddedings for each position. All elements past `lengths` are zero.
+  """
+  batch_size = tf.shape(lengths)[0]
+  embedding = tf.get_variable(
+      "position_embedding",
+      [num_positions, embedding_dim])
+  # Create matrix of positions, mask out positions that are not
+  positions = tf.tile([tf.range(maxlen)], [batch_size, 1])
+  positions_embed = tf.nn.embedding_lookup(embedding, positions)
+  # Mask out positions that are padded
+  positions_mask = tf.sequence_mask(
+      lengths=lengths,
+      maxlen=maxlen,
+      dtype=tf.float32)
+  positions_embed = positions_embed * tf.expand_dims(positions_mask, 2)
+  return positions_embed
+
+
+class PoolingEncoder(Encoder):
+  """An encoder that pools over embeddings, as described in
+  https://arxiv.org/abs/1611.02344. The encoder supports optional positions
+  embeddings and a configurable pooling window.
+
+  Params:
+    pooling_fn: The 1-d pooling function to use, e.g.
+      `tensorflow.layers.average_pooling1d`.
+    pool_size: pooling window size, an inteher
+    strides: stride used for pooling, an integer
+    position_embeddings.enable: If True, add position embeddings to the inputs.
+    position_embeddings.num_positions: The size of the position embeddings.
+      This should be set to the maximum sequence length of the inputs.
+    position_embeddings.combiner_fn: Function to use the position embeddings
+      with the inputs. For example, `tensorflow.add`.
+  """
+
+  def __init__(self, params, mode, name="poolig_encoder"):
+    super(PoolingEncoder, self).__init__(params, mode, name)
+    self._pooling_fn = locate(self.params["pooling_fn"])
+    self._combiner_fn = locate(self.params["position_embeddings.combiner_fn"])
+
+  @staticmethod
+  def default_params():
+    return {
+        "pooling_fn": "tensorflow.layers.average_pooling1d",
+        "pool_size": 5,
+        "strides": 1,
+        "position_embeddings.enable": True,
+        "position_embeddings.combiner_fn": "tensorflow.add",
+        "position_embeddings.num_positions": 100,
+    }
+
+  def encode(self, inputs, sequence_length):
+    if self.params["position_embeddings.enable"]:
+      positions_embed = _create_position_embedding(
+          embedding_dim=inputs.get_shape().as_list()[-1],
+          num_positions=self.params["position_embeddings.num_positions"],
+          lengths=sequence_length,
+          maxlen=tf.shape(inputs)[1])
+      inputs = self._combiner_fn(inputs, positions_embed)
+
+    outputs = self._pooling_fn(
+        inputs=inputs,
+        pool_size=self.params["pool_size"],
+        strides=self.params["strides"],
+        padding="SAME")
+
+    # Final state is the average representation of the pooled embeddings
+    final_state = tf.reduce_mean(outputs, 1)
+
+    return EncoderOutput(
+        outputs=outputs,
+        final_state=final_state,
+        attention_values=inputs)
