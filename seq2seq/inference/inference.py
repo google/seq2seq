65b397e3911d64e047cbb66f90cf15598f626bae
Denny Britz <dennybritz@gmail.com>
2017-03-12 09:41:58 -0700

    Remove unused code

--- seq2seq/inference/inference.py
+++ seq2seq/inference/inference.py
@@ -23,32 +23,6 @@ import tensorflow as tf
 from seq2seq.training import utils as training_utils
 
 
-def create_predictions_iter(predictions_dict, sess):
-  """Runs prediciton fetches in a sessions and flattens batches as needed to
-  return an iterator of predictions. Yield elements until an
-  OutOfRangeError for the feeder queues occurs.
-
-  Args:
-    predictions_dict: The dictionary to be fetched. This will be passed
-      to `session.run`. The first dimensions of each element in this
-      dictionary is assumed to be the batch size.
-    sess: The Session to use.
-
-  Returns:
-    An iterator of the same shape as predictions_dict, but with one
-    element at a time and the batch dimension removed.
-  """
-  with tf.contrib.slim.queues.QueueRunners(sess):
-    while True:
-      try:
-        predictions_ = sess.run(predictions_dict)
-        batch_length = list(predictions_.values())[0].shape[0]
-        for i in range(batch_length):
-          yield {key: value[i] for key, value in predictions_.items()}
-      except tf.errors.OutOfRangeError:
-        break
-
-
 def create_inference_graph(model, input_pipeline, batch_size=32):
   """Creates a graph to perform inference.
 
@@ -65,9 +39,10 @@ def create_inference_graph(model, input_pipeline, batch_size=32):
 
   # TODO: This doesn't really belong here.
   # How to get rid of this?
-  if model.params["inference.beam_search.beam_width"] > 1:
-    tf.logging.info("Setting batch size to 1 for beam search.")
-    batch_size = 1
+  if hasattr(model, "use_beam_search"):
+    if model.use_beam_search:
+      tf.logging.info("Setting batch size to 1 for beam search.")
+      batch_size = 1
 
   input_fn = training_utils.create_input_fn(
       pipeline=input_pipeline,
