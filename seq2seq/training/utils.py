550241992f62cd5c1d0d5c5d06f0278b706bd9a8
Denny Britz <dennybritz@gmail.com>
2017-03-14 17:09:05 -0700

    Fix encoding placement

--- seq2seq/training/utils.py
+++ seq2seq/training/utils.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2017 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# -*- coding: utf-8 -*-
 """Miscellaneous training utility functions.
 """
 
