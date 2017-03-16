412dfbbf9c1eb5b98d69b8303198f265ed527c10
Denny Britz <dennybritz@gmail.com>
2017-03-16 10:58:35 -0700

    Import unicode literals for python 2

--- seq2seq/data/postproc.py
+++ seq2seq/data/postproc.py
@@ -17,6 +17,11 @@
 A collection of commonly used post-processing functions.
 """
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 def strip_bpe(text):
   """Deodes text that was processed using BPE from
   https://github.com/rsennrich/subword-nmt"""
